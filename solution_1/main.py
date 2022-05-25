59f92548e0a780845c604fd9258e906792f7aedc
Omikix <omikix@gmail.com>
2022-05-25 16:31:37 +0200

    testing

--- solution_1/main.py
+++ solution_1/main.py
@@ -1,3 +1,6 @@
+import time
+
+
 def prepare_data(file: str):
     with open(f"zadanie_B\{file}", 'r') as f:
         data = f.readlines()
@@ -9,6 +12,7 @@ def prepare_data(file: str):
 
 
 # print(prepare_data("slo1.in"))
+t = time.time()
 
 
 def sort_by_schema(elephant_number, schema, elephant_dict):
@@ -16,36 +20,49 @@ def sort_by_schema(elephant_number, schema, elephant_dict):
     power = 0
 
     numbers_in_right_position = []
-
+    a = 0
     for i in elephant_number:
+        a += 1
+        print(a)
         if elephant_number.index(i) == schema.index(i):
             numbers_in_right_position.append(i)
-
+    print("=\n\n" * 500)
     while elephant_number != schema:
-        if elephant_number[index] not in numbers_in_right_position:
-            element = elephant_number[index]
-            position_of_element = elephant_number.index(element)
-            right_position_of_element = schema.index(element)
-            if elephant_number[right_position_of_element] == schema[position_of_element]:
-                elephant_number[right_position_of_element], elephant_number[position_of_element] = \
-                    elephant_number[position_of_element], elephant_number[right_position_of_element]
-                power += int(elephant_dict.get(elephant_number[right_position_of_element])) + \
-                         int(elephant_dict.get(elephant_number[position_of_element]))
-                numbers_in_right_position.extend([elephant_number[right_position_of_element],
-                                                  elephant_number[position_of_element]])
-            elif elephant_number[right_position_of_element] != schema[position_of_element]:
-                elephant_number[right_position_of_element], elephant_number[position_of_element] =\
-                    elephant_number[position_of_element], elephant_number[right_position_of_element]
-                power += int(elephant_dict.get(elephant_number[right_position_of_element])) + \
-                         int(elephant_dict.get(elephant_number[position_of_element]))
-                numbers_in_right_position.append(elephant_number[right_position_of_element])
+        if index < len(elephant_number):
+            if elephant_number[index] not in numbers_in_right_position:
+                element = elephant_number[index]
+                position_of_element = elephant_number.index(element)
+                right_position_of_element = schema.index(element)
+                if elephant_number[right_position_of_element] == schema[position_of_element]:
+                    elephant_number[right_position_of_element], elephant_number[position_of_element] = \
+                        elephant_number[position_of_element], elephant_number[right_position_of_element]
+                    power += int(elephant_dict.get(elephant_number[right_position_of_element])) + \
+                             int(elephant_dict.get(elephant_number[position_of_element]))
+                    numbers_in_right_position.extend([elephant_number[right_position_of_element],
+                                                      elephant_number[position_of_element]])
+                    index += 1
+                elif elephant_number[right_position_of_element] != schema[position_of_element]:
+                    elephant_number[right_position_of_element], elephant_number[position_of_element] =\
+                        elephant_number[position_of_element], elephant_number[right_position_of_element]
+                    power += int(elephant_dict.get(elephant_number[right_position_of_element])) + \
+                             int(elephant_dict.get(elephant_number[position_of_element]))
+                    numbers_in_right_position.append(elephant_number[right_position_of_element])
+                    index += 1
+                else:
+                    index = 0
+                print(len(elephant_number) - len(numbers_in_right_position))
+
             else:
-                index = 0
+                index += 1
         else:
-            index += 1
+            index = 0
     return power
 
 
-a, b, c = prepare_data("slo1.in")
-
+a, b, c = prepare_data("slo8a.in")
+print("=" * 200)
 print(sort_by_schema(a, b, c))
+
+r = time.time()
+
+print(r - t)
--- solution_1/test.py
+++ solution_1/test.py
@@ -1,40 +1,58 @@
-# some_list = [2, 4, 1, 6, 3, 5, ]
-# schema = [4, 3, 6, 1, 2, 5, ]
-
-a = [3015, 4728, 4802, 4361, 135, 4444, 4313, 1413, 4581, 546]
-b = [4444, 135, 546, 3015, 4802, 4581, 4728, 1413, 4361, 4313]
+from tqdm import tqdm
+import time
 
 
-def sort_by_schema(some_list, schema):
-    index = 0
+def prepare_data(file: str):
+    with open(f"zadanie_B\{file}", 'r') as f:
+        data = f.readlines()
+        schema = data[3].split()
+        elephant_number = data[2].split()
+        elephant_weight = data[1].split()
+        elephant_dict = {elephant_number[index]: elephant_weight[index] for index in range(len(elephant_number))}
+    return elephant_number, schema, elephant_dict
+
+
+# print(prepare_data("slo1.in"))
+time_start = time.time()
+
+
+def sort_by_schema(elephant_number, schema, elephant_dict):
     power = 0
+    numbers_in_wrong_position = []
+    print("Your list with elephants is loading...")
+    for i in tqdm(elephant_number):
+        if elephant_number.index(i) != schema.index(i):
+            numbers_in_wrong_position.append(i)
+    print("\nPlease wait, your list is sorting now...\n")
 
-    numbers_in_right_position = []
-
-    for i in some_list:
-        if some_list.index(i) == schema.index(i):
-            numbers_in_right_position.append(i)
-
-    while some_list != schema:
-        if some_list[index] not in numbers_in_right_position:
-            element = some_list[index]
-            position_of_element = some_list.index(element)
+    while elephant_number != schema:
+        for element in numbers_in_wrong_position:
+            position_of_element = elephant_number.index(element)
             right_position_of_element = schema.index(element)
-            if some_list[right_position_of_element] == schema[position_of_element]:
-                some_list[right_position_of_element], some_list[position_of_element] = some_list[position_of_element],\
-                                                                                       some_list[right_position_of_element]
-                power += some_list[right_position_of_element] + some_list[position_of_element]
-                numbers_in_right_position.extend([some_list[right_position_of_element], some_list[position_of_element]])
-            elif some_list[right_position_of_element] != schema[position_of_element]:
-                some_list[right_position_of_element], some_list[position_of_element] = some_list[position_of_element], \
-                                                                                       some_list[right_position_of_element]
-                power += some_list[right_position_of_element] + some_list[position_of_element]
-                numbers_in_right_position.append(some_list[right_position_of_element])
-            else:
-                index = 0
-        else:
-            index += 1
-    return some_list, power
+            if elephant_number[right_position_of_element] == schema[position_of_element]:
+                elephant_number[right_position_of_element], elephant_number[position_of_element] = \
+                    elephant_number[position_of_element], elephant_number[right_position_of_element]
+                power += int(elephant_dict.get(elephant_number[right_position_of_element])) + \
+                         int(elephant_dict.get(elephant_number[position_of_element]))
+                numbers_in_wrong_position.pop(
+                    numbers_in_wrong_position.index(elephant_number[right_position_of_element])
+                )
+                numbers_in_wrong_position.pop(numbers_in_wrong_position.index(elephant_number[position_of_element]))
+            elif elephant_number[right_position_of_element] != schema[position_of_element]:
+                elephant_number[right_position_of_element], elephant_number[position_of_element] = \
+                    elephant_number[position_of_element], elephant_number[right_position_of_element]
+                power += int(elephant_dict.get(elephant_number[right_position_of_element])) + \
+                         int(elephant_dict.get(elephant_number[position_of_element]))
+                numbers_in_wrong_position.pop(
+                    numbers_in_wrong_position.index(elephant_number[right_position_of_element])
+                )
+        # print(len(numbers_in_wrong_position))
+    return power
 
 
-print(sort_by_schema(a, b))
+a, b, c = prepare_data("slo8a.in")
+print(f"Power needed to move elephant: {sort_by_schema(a, b, c)}\n")
+
+time_end = time.time()
+
+print(f"Operation took {time_end - time_start:.2f} seconds\n")
